6c12a659d23703569e0bbee0d21c82bc1d15b393
Osein <osein.wtr@gmail.com>
2022-06-05 12:29:27 +0300

    added cocoapods

--- Entities/AppDelegate.swift
+++ Entities/AppDelegate.swift
@@ -9,9 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
     
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Entities' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Entities
+  pod 'Moya', '~> 15.0'
+  pod 'Swinject'
+  pod 'ReachabilitySwift'
+  pod 'SVProgressHUD'
+
+  target 'EntitiesTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
