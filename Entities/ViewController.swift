c03a452a21241ac0c6a0cef89fddf6079a5a4b6d
Osein <osein.wtr@gmail.com>
2022-06-05 12:23:31 +0300

    Initial commit

---
+++ Entities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Entities
+//
+//  Created by Hasolas on 5.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
